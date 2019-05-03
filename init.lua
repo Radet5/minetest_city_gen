f46189b98c329d817da10a2c15cb1babbe1cffae
Radet5 <radientradet5@gmail.com>
2019-05-03 12:02:59 -0400

    Fixed peak memory issues, made city wall building work segment at a time, made city wall thicker

--- init.lua
+++ init.lua
@@ -125,7 +125,9 @@ local t = f:read("*all")
 f:close()
 local wall_table = json:decode(t)
 
-Structure = {s_type="structure", low_z = math.huge, high_z = -math.huge, low_x = math.huge, high_x = -math.huge, size_y=9}
+schem_filename = "mods/minetest_city_gen/check_wall_schems"
+
+Structure = {s_type="structure", low_z = math.huge, high_z = -math.huge, low_x = math.huge, high_x = -math.huge, size_y=9, scale=scale}
 
 function Structure:new (o)
 	o = o or {}
@@ -137,8 +139,8 @@ function Structure:new (o)
 end
 
 function Structure:insert_point(px,pz)
-	local x=math.floor(px*scale)
-	local z=math.floor(pz*scale)
+	local x=math.floor(px*self.scale)
+	local z=math.floor(pz*self.scale)
 	table.insert(self.verticies, {x=x,z=z})
 	if z > self.high_z then self.high_z = z end
 	if z < self.low_z then self.low_z = z end
@@ -188,27 +190,45 @@ function Structure:get_verticies_count()
 end
 
 function Structure:plot(z, y, x, node_type)
+	local schem_file = assert(io.open(schem_filename, "a"))
 	if self.defined_nodes == nil then self.defined_nodes = {} end
 	if self.defined_nodes[z] then
 		if self.defined_nodes[z][y] then
 			if self.defined_nodes[z][y][x] then
 				--io.stderr:write("node (" .. z .. "," .. y .. "," .. z .. ") multiple defined\n")
 				self.defined_nodes[z][y][x] = {node=node_type}
+				--schem_file:write("redefining: "..z.." "..y.." "..x.."\n")
 			else
 				self.defined_nodes[z][y][x] = {node=node_type}
+				--schem_file:write("defining: "..z.." "..y.." "..x.."<\n")
 			end
 		else 
 			self.defined_nodes[z][y] = {x=x}
 			self.defined_nodes[z][y][x] = {node=node_type}
+			--schem_file:write("defining: "..z.." "..y.."<"..x.."<\n")
 		end
 	else
 		self.defined_nodes[z] = {y=y}
 		self.defined_nodes[z][y] = {x=x}
 		self.defined_nodes[z][y][x] = {node=node_type}
+		--schem_file:write("defining: "..z.."<"..y.."<"..x.."<\n")
 	end
+	schem_file:close()
 end
 
-function Structure:plotLineLow(x0, y0, x1, y1, level, node_type)
+function Structure:plotBlock(z, y, x, node_type)
+	self:plot(z, y, x, node_type)
+	self:plot(z-1, y, x-1, node_type)
+	self:plot(z, y, x-1, node_type)
+	self:plot(z+1, y, x-1, node_type)
+	self:plot(z+1, y, x, node_type)
+	self:plot(z+1, y, x+1, node_type)
+	self:plot(z, y, x+1, node_type)
+	self:plot(z-1, y, x+1, node_type)
+	self:plot(z-1, y, x, node_type)
+end
+
+function Structure:plotLineLow(x0, y0, x1, y1, level, node_type, thick)
 	local dx = x1 - x0
 	local dy = y1 - y0
 	local yi = 1
@@ -220,16 +240,10 @@ function Structure:plotLineLow(x0, y0, x1, y1, level, node_type)
 	local y = y0
 
 	for x = x0,x1 do
-		self:plot(y, level, x, node_type)
-		if self.s_type == "streets" or self.s_type == "Wall" then
-			self:plot(y-1, level, x-1, node_type)
-			self:plot(y, level, x-1, node_type)
-			self:plot(y+1, level, x-1, node_type)
-			self:plot(y+1, level, x, node_type)
-			self:plot(y+1, level, x+1, node_type)
-			self:plot(y, level, x+1, node_type)
-			self:plot(y-1, level, x+1, node_type)
-			self:plot(y-1, level, x, node_type)
+		if thick then
+			self:plotBlock(y, level, x, node_type)
+		else
+			self:plot(y, level, x, node_type)
 		end
 		if D > 0 then
 			y = y + yi
@@ -239,7 +253,7 @@ function Structure:plotLineLow(x0, y0, x1, y1, level, node_type)
 	end
 end
 
-function Structure:plotLineHigh(x0, y0, x1, y1, level, node_type)
+function Structure:plotLineHigh(x0, y0, x1, y1, level, node_type, thick)
 	local dx = x1 - x0
 	local dy = y1 - y0
 	local xi = 1
@@ -252,7 +266,11 @@ function Structure:plotLineHigh(x0, y0, x1, y1, level, node_type)
 
 	for y = y0,y1 do
 		--io.stderr:write(" by ("..x..","..y..")\n")
-		self:plot(y, level, x, node_type)
+		if thick then
+			self:plotBlock(y, level, x, node_type)
+		else
+			self:plot(y, level, x, node_type)
+		end
 		if D > 0 then
 			x = x + xi
 			D = D - 2*dy
@@ -260,44 +278,45 @@ function Structure:plotLineHigh(x0, y0, x1, y1, level, node_type)
 		D = D + 2*dx
 	end
 end
-function Structure:plotLine(org_y, nxt_y, org_x, nxt_x, level, node_type)
+function Structure:plotLine(org_y, nxt_y, org_x, nxt_x, level, node_type, thick)
 	if math.abs(nxt_y - org_y) < math.abs(nxt_x - org_x) then
 		if org_x > nxt_x then
-			self:plotLineLow(nxt_x, nxt_y, org_x, org_y, level, node_type)
+			self:plotLineLow(nxt_x, nxt_y, org_x, org_y, level, node_type, thick)
 		else
-			self:plotLineLow(org_x, org_y, nxt_x, nxt_y, level, node_type)
+			self:plotLineLow(org_x, org_y, nxt_x, nxt_y, level, node_type, thick)
 		end
 	else
 		if org_y > nxt_y then
-			self:plotLineHigh(nxt_x, nxt_y, org_x, org_y, level, node_type)
+			self:plotLineHigh(nxt_x, nxt_y, org_x, org_y, level, node_type, thick)
 		else
-			self:plotLineHigh(org_x, org_y, nxt_x, nxt_y, level, node_type)
+			self:plotLineHigh(org_x, org_y, nxt_x, nxt_y, level, node_type, thick)
 		end
 	end
 end
 
-function Structure:buildPerimeter(bottom, top, node_type)
+function Structure:buildPerimeter(bottom, top, node_type, thick)
 	for y =bottom,top do
 		for i = 1,self:get_verticies_count() do
-			--print(inspect(self:get_x(i)))
 			local nxt_x = self:get_x(1)
 			local nxt_z = self:get_z(1)
 			if i < #self.verticies then
 				nxt_x = self:get_x(i+1)
 				nxt_z = self:get_z(i+1)
 			end
-			self:plotLine(self:get_z(i), nxt_z, self:get_x(i), nxt_x, y, node_type)
+			self:plotLine(self:get_z(i), nxt_z, self:get_x(i), nxt_x, y, node_type, thick)
 		end
 	end
 end
 
-function Structure:buildPath(bottom, top, node_type)
+function Structure:buildPath(bottom, top, node_type, thick)
+	local nxt_x = -1
+	local nxt_z = -1
 	for y =bottom,top do
 		for i = 1,self:get_verticies_count()-1 do
 			--print(inspect(self:get_x(i)))
 			nxt_x = self:get_x(i+1)
 			nxt_z = self:get_z(i+1)
-			self:plotLine(self:get_z(i), nxt_z, self:get_x(i), nxt_x, y, node_type)
+			self:plotLine(self:get_z(i), nxt_z, self:get_x(i), nxt_x, y, node_type, thick)
 		end
 	end
 end
@@ -376,7 +395,7 @@ end
 
 function Structure:build ()
 	if self.s_type == "streets" then
-		self:buildPath(0, 0, "artdeco:stonewall")
+		self:buildPath(0, 0, "artdeco:stonewall", true)
   	elseif self.s_type == "Slum" then
 		self.size_y = 4
 		self:buildPerimeter(0,4,"cottages:loam")
@@ -407,36 +426,48 @@ function Structure:build ()
 --		for i = 0,self:get_size_y() do
 --			self:buildFillPlatform(i, "air")
 --		end
-	elseif self.s_type ~= "Wall" then
+	elseif self.s_type == "Wall_Seg" then
+		self:buildPath(0, self:get_size_y(), "default:cobble", true)
+	--	local schem_file = assert(io.open(schem_filename, "a"))
+	--	schem_file:write(inspect(self.defined_nodes).."\n")
+	--	schem_file:close()
+	elseif self.s_type == "Wall" then
+		self:buildPerimeter(0, self:get_size_y(), "default:cobble", true)
+
+	else
 		self:buildPerimeter(0, 5, "default:wood")
 		self:buildPerimeter(5, self:get_size_y(), "cottages:loam")
 		self:buildFillPlatform(0, "default:stone")
 		self:buildFillPlatform(4, "default:tree")
 		self:buildFillPlatform(self:get_size_y(), "default:wood")
 		self:buildAddDoors("doors:door_wood_a")
-	else
-		self:buildPerimeter(0, self:get_size_y(), "default:cobble")
-		self:buildFillPlatform(0, "default:dirt")
 	end
 end
 
 function Structure:get_schematic ()
-	local schematic = {size={x=self:get_size_x()+1,y=self:get_size_y()+1,z=self:get_size_z()+1},data={}, yslice_prob={}}
-	for i = 0,self.size_y do
-		table.insert(schematic.yslice_prob, {ypos=tostring(i), prob="254"})
-	end
-	for z = 0,self:get_size_z() do
+	local size_x = self:get_size_x()
+	local size_z = self:get_size_z()
+	local size_y = self:get_size_y()
+	local schematic = {size={},data={}, yslice_prob={}}
+	schematic.size={x=self:get_size_x()+1,y=self:get_size_y()+1,z=self:get_size_z()+1}
+	for z = 0,size_z do
 		for y =0,self:get_size_y() do
-			for x =0,self:get_size_x() do
+			for x =0,size_x do
 				local name = ""
 				local prob = "254"
 				if pcall(function() name = self.defined_nodes[z][y][x].node end) == false then 
+					--local schem_file = assert(io.open(schem_filename, "a"))
+					--schem_file:write(z..","..y..","..x.."\n")
+					--schem_file:close()
 					name = "air"
-					if self.s_type ~= "Wall" then prob = "0" end
+					if self.s_type ~= "Wall" and self.s_type ~= "Wall_Seg" then prob = "0" end
 				end
 				table.insert(schematic.data, {name = name, prob=prob, param2="0"})
 			end
 		end
+	end
+	for i = 0,self.size_y do
+		table.insert(schematic.yslice_prob, {ypos=tostring(i), prob="254"})
 	end
 	return schematic
 end
@@ -470,7 +501,19 @@ local high_y = -math.huge
 local low_x = math.huge
 local high_x = -math.huge
 
+function rec_mem(note)
+	local mem_filename = "mods/minetest_city_gen/mem_usage"
+	local mem_file = assert(io.open(mem_filename, "a"))
+	mem_file:write(collectgarbage('count'))
+	mem_file:write(note)
+	mem_file:close()
+end
 
+local mem_filename = "mods/minetest_city_gen/mem_usage"
+local mem_file = assert(io.open(mem_filename, "w"))
+mem_file:write(collectgarbage('count'))
+mem_file:write(" before\n")
+mem_file:close()
 for path_type,paths_data in pairs(path_table) do
 	for _,points in pairs(paths_data) do
 		path = Structure:new({s_type=path_type})
@@ -480,6 +523,7 @@ for path_type,paths_data in pairs(path_table) do
 		table.insert(paths,path)
 	end
 end
+rec_mem(" after paths\n")
 
 for k,v in pairs(building_table) do
 	for ward,points in pairs(v) do
@@ -501,6 +545,8 @@ for k,v in pairs(building_table) do
 		table.insert(structures,structure)
 	end
 end
+rec_mem(" after buildings\n")
+
 local z = 0
 local y = 0
 local x = 0
@@ -517,86 +563,60 @@ for i,point in pairs(wall_table) do
 	if y > high_y then high_y = y end
 	if y < low_y then low_y = y end
 end
---air = {}
---air["1"] = Structure:new({s_type="air"})
---air["2"] = Structure:new({s_type="air"})
---air["3"] = Structure:new({s_type="air"})
---air["4"] = Structure:new({s_type="air"})
---
---air["1"].size_y = 9
---air["2"].size_y = 9
---air["3"].size_y = 9
---air["4"].size_y = 9
---
---air["1"]:insert_point(low_x, low_z)
---air["1"]:insert_point(math.floor(high_x/2), low_z)
---air["1"]:insert_point(math.floor(high_x/2), math.floor(high_z/2))
---air["1"]:insert_point(math.floor(high_x/2), math.floor(high_z/2))
---
---air["2"]:insert_point(math.ceil(high_x/2), math.floor(low_z))
---air["2"]:insert_point(high_x, low_z)
---air["2"]:insert_point(high_x, math.floor(high_z/2))
---air["2"]:insert_point(math.ceil(high_x/2), math.floor(high_z/2))
---
---air["3"]:insert_point(math.ceil(high_x/2), math.ceil(high_z/2))
---air["3"]:insert_point(high_x, math.ceil(high_z/2))
---air["3"]:insert_point(high_x, high_z)
---air["3"]:insert_point(math.ceil(high_x/2), high_z)
---
---air["4"]:insert_point(math.floor(high_x/2), high_z)
---air["4"]:insert_point(low_x, high_z)
---air["4"]:insert_point(low_x, math.ceil(high_z/2))
---air["4"]:insert_point(math.floor(high_x/2), math.ceil(high_z/2))
---
---param = "1"
---air[param]:build()
---air_schem_id[param] = minetest.register_schematic(air[param]:get_schematic())
---air[param].defined_nodes = nil
---param = "2"
---air[param]:build()
---air_schem_id[param] = minetest.register_schematic(air[param]:get_schematic())
---air[param].defined_nodes = nil
---param = "3"
---air[param]:build()
---air_schem_id[param] = minetest.register_schematic(air[param]:get_schematic())
---air[param].defined_nodes = nil
---param = "4"
---air[param]:build()
---air_schem_id[param] = minetest.register_schematic(air[param]:get_schematic())
---air[param].defined_nodes = nil
---for i,structure in pairs(structures) do
---	--print(inspect(structure))
---	local draw = {}
---	for z = 0,structure:get_size_z() do
---		draw[z] = {}
---		for x = 0,structure:get_size_x() do
---			draw[z][x] = "-"
---		end
---	end
---	for i,v in pairs(structure.verticies) do
---		--print("("..v.x..","..v.z..") = ("..structure:get_x(i)..","..structure:get_z(i)..")")
---		draw[structure:get_z(i)][structure:get_x(i)] = "X"
---	end
---	for z = 0,structure:get_size_z() do
---		for x = 0,structure:get_size_x() do
---			--io.stderr:write(draw[z][x])
---		end
---		--io.stderr:write("\n")
---	end
---end
 
 local place_x = -620
 local place_y = 0
 local place_z = 590
+print("make a wall")
+wall.schem_ids = {}
+wall_segs = {}
+local schem_filename = "mods/minetest_city_gen/check_wall_schems"
+local schem_file = assert(io.open(schem_filename, "w"))
+schem_file:close()
+for i = 1,wall:get_verticies_count()-1 do
+	--wall:build(i)
+	local segment = Structure:new({s_type="Wall_Seg", size_y = 15, scale = 1})
+	local nxt_i = 1
+	if i < #wall.verticies then nxt_i = i + 1 end
+	segment:insert_point(wall.verticies[i].x, wall.verticies[i].z)
+	segment:insert_point(wall.verticies[nxt_i].x, wall.verticies[nxt_i].z)
+	table.insert(wall_segs, segment)
+	wall_segs[i]:build()
+	rec_mem(" after build wall seg\n")
+	local seg_schem = wall_segs[i]:get_schematic()
+	
+	wall_segs[i].schem_id = minetest.register_schematic(seg_schem)
+	schem_file = assert(io.open(schem_filename, "a"))
+--schem_file:write("("..segment.low_x..","..segment.low_z..") ->".."("..segment:get_global_x(low_x)..","..segment:get_global_z(low_z)..") ->("..place_x +segment:get_global_x(low_x)..","..place_z +segment:get_global_z(low_z)..")\n")
+	schem_file:close()
+	rec_mem(" after schem seg\n")
+	seg_schem = nil
+	wall_segs[i].defined_nodes = nil
+	collectgarbage()
+	wall_segs[i].defined_nodes = {}
+	rec_mem(" after garbage collect def_nodes\n")
+end
+for i,path in pairs(paths) do
+	if path.s_type == "streets" then
+		path:build()
+		path.schem_id = minetest.register_schematic(path:get_schematic())
+		path.defined_nodes = nil
+	end
+end
+rec_mem(" after streets\n")
+collectgarbage()
+rec_mem(" after garbage streets\n")
 local schem_count = #structures
 print("Building Count: "..#structures)
-local schematic_ids = {}
 for i = 1,schem_count do
 	structures[i]:build()
 	structures[i].schem_id = minetest.register_schematic(structures[i]:get_schematic())
 	structures[i].defined_nodes = nil
 end
+rec_mem(" after structures\n")
 collectgarbage()
+rec_mem(" after garbagecollect\n")
+
 minetest.register_chatcommand("place", {func = function (name, param) 
 	for i =1,schem_count do
 		local schem_x = place_x + structures[i]:get_global_x(low_x)
@@ -604,38 +624,38 @@ minetest.register_chatcommand("place", {func = function (name, param)
 		local schem_z = place_z + structures[i]:get_global_z(low_z)
 		print(dump(minetest.place_schematic({x=schem_x,y=schem_y,z=schem_z}, structures[i].schem_id, 0, {}, true)))
 	end
+	rec_mem(" after place buildings\n")
 	return true
+
 end})
 --minetest.register_chatcommand("clear", {func = function (name, param) 
 --	minetest.place_schematic({x=place_x,y=place_y,z=place_z}, air_schem_id[param], 0, {}, true)
 --	return true
 --end})
 minetest.register_chatcommand("wall", {func = function (name, param) 
-	if wall.defined_nodes ~= nil then
-		print("make a wall")
-		wall:build()
-		wall.schem_id = minetest.register_schematic(wall:get_schematic())
-		wall.defined_nodes = nil
-		collectgarbage()
+	local schems = {}
+	rec_mem(" before place wall\n")
+	for i,seg in pairs(wall_segs) do
+		local schem_x = place_x + seg:get_global_x(low_x)
+		local schem_y = place_y
+		local schem_z = place_z + seg:get_global_z(low_z)
+		table.insert(schems, {schem_x, schem_y, schem_z})
+		print(dump(minetest.place_schematic({x=schem_x,y=schem_y,z=schem_z}, seg.schem_id, 0, {}, true)))
 	end
-	minetest.place_schematic({x=place_x,y=place_y,z=place_z}, wall.schem_id, 0, {}, true)
-	return true
+	rec_mem(" after place wall\n")
+	return true,inspect(schems)
 end})
 minetest.register_chatcommand("streets", {func = function (name, param) 
 	local street_ids = {}
 	for i,path in pairs(paths) do
 		if path.s_type == "streets" then
-			if path.defined_nodes ~= nil then
-				path:build()
-				path.schem_id = minetest.register_schematic(path:get_schematic())
-				path.defined_nodes = nil
-			end
 			local schem_x = place_x + path:get_global_x(low_x)
 			local schem_y = place_y
 			local schem_z = place_z + path:get_global_z(low_z)
 			print(dump(minetest.place_schematic({x=schem_x,y=schem_y,z=schem_z}, path.schem_id, 0, {}, true)))
 		end
 	end
+	rec_mem(" after place streets\n")
 	return true
 end})
 local filename = "small_schem.txt"
